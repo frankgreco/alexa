9ab2cc83890f8626f8c0b62befe57df891f9fb41
Frank Greco Jr <frankgreco@northwesternmutual.com>
2017-01-17 23:53:12 -0600

    changes

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+SKILLS_DIR=$PWD
+
+for SKILL in skills/*/ ; do
+    cd $SKILL/src
+    FUNCTION_NAME=$(cat package.json | jq -r '.name')
+    ARN=arn:aws:lambda:$AWS_DEFAULT_REGION:$ACCOUNT_NUMBER:function:$FUNCTION_NAME
+    npm install
+    zip -r $FUNCTION_NAME.zip index.js node_modules/
+    aws lambda update-function-code --function-name $ARN --zip-file fileb://$FUNCTION_NAME.zip --publish
+    cd $SKILLS_DIR
+done
